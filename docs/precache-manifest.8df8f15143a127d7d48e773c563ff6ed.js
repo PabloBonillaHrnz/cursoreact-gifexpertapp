726d30c091a8cfdf690ddde5fd0df3e297ab47b9
Pablo Bonilla <pablo1597@hotmail.com>
2020-07-25 13:00:43 -0500

    Docs agregado

---
+++ docs/precache-manifest.8df8f15143a127d7d48e773c563ff6ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9350fe930b53724cb522f5e44c7a1224",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e21083d1acc6af5b5e13",
+    "url": "/static/css/main.352501b0.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e21083d1acc6af5b5e13",
+    "url": "/static/js/main.60a9e1ec.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
